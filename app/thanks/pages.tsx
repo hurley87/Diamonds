d0d04aec821138c3a274fb2e60d289b72d712813
David Hurley <dhurls99@gmail.com>
2024-06-20 20:30:58 -0400

    copy udpate

--- app/thanks/pages.tsx
+++ app/thanks/pages.tsx
@@ -3,8 +3,8 @@ export default function ThanksPage() {
     <div className="p-6 flex flex-col gap-12 font-serif max-w-md mx-auto">
       <p>Thanks!</p>
       <p>
-        It'll take Coinbase 5 - 10 minutes to confirm your payment. Once
-        confirmed, you'll see your Diamond in your profile.
+        {`It'll take Coinbase 5 - 10 minutes to confirm your payment. Once
+        confirmed, you'll see your Diamond in your profile.`}
       </p>
       <p>Email us if you have a question.</p>
     </div>
