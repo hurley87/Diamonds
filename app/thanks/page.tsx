d3fe3ce5b7223b41655b598ab9a0fb99e4b3317a
David Hurley <dhurls99@gmail.com>
2025-03-15 13:05:04 -0400

    finish thanks page

--- app/thanks/page.tsx
+++ app/thanks/page.tsx
@@ -9,33 +9,41 @@ export default function ThanksPage() {
   const address = user?.wallet?.address as `0x${string}`;
 
   return (
-    <div className="w-fit max-w-screen-2xl mx-auto flex flex-col md:flex-row p-6 md:p-24 gap-12 md:gap-24">
-      <div className="flex flex-col h-full justify-start">
-        <h1
-          className={`text-5xl leading-[120%] w-full max-w-[420px] ${serif.className}`}
-        >
-          Awesome.
-          <br />
-          You just purchased the diamond!
-        </h1>
-      </div>
-      <div className="flex flex-col gap-14">
-        <p className="text-xl leading-[40px] max-w-xl opacity-80">
-          Thank you for your purchase. Coinbase will take five to fifteen
-          minutes to confirm your payment. Please check your email, including
-          your spam folder, for the confirmation email. If you do not receive
-          your confirmation email or diamond within thirty minutes, please
-          contact us via{' '}
-          <Link className="underline" href="mailto: support@4collection.com">
-            email
+    <div className="flex flex-col gap-12 py-12 max-w-screen-xl mx-auto">
+      <img src="/steps4.png" alt="Diamond" className="w-full h-auto" />
+      <div className="flex flex-col md:flex-row p-6 md:p-24 gap-12 md:gap-24">
+        <div className="flex flex-col h-full justify-start">
+          <h1
+            className={`text-6xl leading-[120%] w-full max-w-[520px] ${serif.className} uppercase`}
+          >
+            Awesome.
+            <br />
+            YOU JUST PURCHASED THE DIAMOND!
+          </h1>
+        </div>
+        <div className="flex flex-col gap-14">
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl uppercase text-[#7A8385]">WHAT NOW?</h2>
+            <p className="text-xl leading-[40px] max-w-xl opacity-80">
+              The digital asset already assigned to your wallet. Consectetur
+              adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis
+              tellus. Sed dignissim, metus nec fringilla accumsan.
+            </p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl uppercase text-[#7A8385]">WHAT’S NEXT?</h2>
+            <p className="text-xl leading-[40px] max-w-xl opacity-80">
+              Qorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+              turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+              nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+              tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+              feugiat lectus.
+            </p>
+          </div>
+          <Link href={`/profile/${address}`}>
+            <Button text="Check out your diamond" />
           </Link>
-          . Click the button below to check if your diamond has arrived. Your
-          individual GIA certificate will be updated with the metadata within
-          the next 24-48 hours.
-        </p>
-        <Link href={` /profile/${address}`}>
-          <Button text="Check out your diamond" />
-        </Link>
+        </div>
       </div>
     </div>
   );
