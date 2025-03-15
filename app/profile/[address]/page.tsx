bf2e302336114c07e1a0bf59b02baf7ee90e5b0a
David Hurley <dhurls99@gmail.com>
2025-03-15 13:43:07 -0400

    update profile page

--- app/profile/[address]/page.tsx
+++ app/profile/[address]/page.tsx
@@ -14,22 +14,24 @@ export default async function UpdatePage({ params }: UpdatePageProps) {
   };
 
   return (
-    <div className="p-6 flex flex-col gap-9 font-serif max-w-3xl w-full mx-auto">
+    <div className="p-6 flex flex-col gap-9 font-serif max-w-3xl w-full mx-auto py-24">
       <div className="flex items-center gap-4">
         <img
           src={`https://zora.co/api/avatar/${address}?size=36`}
           alt="Profile"
-          width={104}
-          height={104}
+          width={60}
+          height={60}
           className="rounded-full"
         />
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-1">
           <Email address={address} />
-          <div className="text-sm">{formatAddress(address)}</div>
+          <div className="text-lg text-[#758585]">{formatAddress(address)}</div>
         </div>
       </div>
       <div className="border-b border-[#B3B3B3]">
-        <h2 className="border-b-2 w-fit border-white pb-2">Your Diamonds</h2>
+        <h2 className="border-b-4 w-fit border-black pb-2 text-[#B3B3B3]">
+          Your Diamonds
+        </h2>
       </div>
       <Tokens address={address} />
     </div>
